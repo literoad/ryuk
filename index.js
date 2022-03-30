3a098414b5f74b9b2b8f76fced2d66ba760c943f
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-03-30 21:26:21 +0300

    correctly format serverless output

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const lighthouse = require('lighthouse')
-const puppeteer = require('puppeteer')
+const lighthouse = require("lighthouse");
+const puppeteer = require("puppeteer");
 
 module.exports.handler = async (event) => {
-  console.log('Received a new request', event)
+  console.log("Received a new request", event);
 
   const body = JSON.parse(event.body);
 
   if (!body.url) {
-    throw new Error('"url" field is required')
+    throw new Error('"url" field is required');
   }
 
-  console.log('Starting browser...')
+  console.log("Starting browser...");
   const browser = await puppeteer.launch({
     defaultViewport: null,
-  })
-  console.log('Started browser!')
+  });
+  console.log("Started browser!");
 
   const options = {
+    output: "json",
     port: new URL(browser.wsEndpoint()).port,
-  }
+  };
 
-  console.log('Starting Lighthouse...')
-  const result = await lighthouse(body.url, options)
-  console.log('Lighthouse finished auditing the page!')
+  console.log("Starting Lighthouse...");
+  const result = await lighthouse(body.url, options);
+  console.log("Lighthouse finished auditing the page!");
 
-  console.log('Report is done for', result.lhr.finalUrl)
+  console.log("Report is done for", result.lhr.finalUrl);
 
-  await browser.close()
+  await browser.close();
 
-  return result.lhr
-}
+  return {
+    statusCode: 200,
+    body: result.report,
+  };
+};
